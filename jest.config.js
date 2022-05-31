2d4e52bb4c5d4b83aac82213ed2a5b3fbfe5f282
David Vílchez <dvilchez@xuaps.com>
2022-05-31 19:08:39 +0200

    Add template

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+// Or async function
+module.exports = async () => {
+  return {
+    testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
+    testEnvironment: 'jsdom',
+    moduleNameMapper: {
+      '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+        '<rootDir>/__mocks__/fileMock.js',
+      '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+    },
+  };
+};
