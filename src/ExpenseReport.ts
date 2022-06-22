e45750bcd0e87d8163dbe0536fae2096b27730b7
David Vílchez <dvilchez@xuaps.com>
2022-06-22 20:47:01 +0200

    Replace switch with dictionary

--- src/ExpenseReport.ts
+++ src/ExpenseReport.ts
@@ -1,4 +1,4 @@
-type ExpenseType = "dinner" | "breakfast" | "car-rental"
+type ExpenseType = 'dinner' | 'breakfast' | 'car-rental'
 
 class Expense {
   type: ExpenseType
@@ -9,40 +9,44 @@ class Expense {
   }
 }
 
+type Names = { [key in 'dinner' | 'breakfast' | 'car-rental']: string }
+
+const getExpenseName = (key: keyof Names): string => {
+  const names: Names = {
+    dinner: 'Dinner',
+    breakfast: 'Breakfast',
+    'car-rental': 'Car Rental',
+  }
+
+  return names[key]
+}
+
 function printReport(expenses: Expense[]) {
   let totalExpenses: number = 0
   let mealExpenses: number = 0
 
-  process.stdout.write("Expenses: " + new Date().toISOString().substr(0, 10) + "\n")
-
+  process.stdout.write('Expenses: ' + new Date().toISOString().substr(0, 10) + '\n')
 
   for (const expense of expenses) {
-    if (expense.type == "dinner" || expense.type == "breakfast") {
+    if (expense.type == 'dinner' || expense.type == 'breakfast') {
       mealExpenses += expense.amount
     }
 
-    let expenseName = ""
-    switch (expense.type) {
-      case "dinner":
-        expenseName = "Dinner"
-        break
-      case "breakfast":
-        expenseName = "Breakfast"
-        break
-      case "car-rental":
-        expenseName = "Car Rental"
-        break
-    }
+    let expenseName = getExpenseName(expense.type)
 
-    let mealOverExpensesMarker = expense.type == "dinner" && expense.amount > 5000 || expense.type == "breakfast" && expense.amount > 1000 ? "X" : " "
+    let mealOverExpensesMarker =
+      (expense.type == 'dinner' && expense.amount > 5000) ||
+      (expense.type == 'breakfast' && expense.amount > 1000)
+        ? 'X'
+        : ' '
 
-    process.stdout.write(expenseName + "\t" + expense.amount + "\t" + mealOverExpensesMarker + "\n")
+    process.stdout.write(expenseName + '\t' + expense.amount + '\t' + mealOverExpensesMarker + '\n')
 
     totalExpenses += expense.amount
   }
 
-  process.stdout.write("Meal Expenses: " + mealExpenses + "\n")
-  process.stdout.write("Total Expenses: " + totalExpenses + "\n")
+  process.stdout.write('Meal Expenses: ' + mealExpenses + '\n')
+  process.stdout.write('Total Expenses: ' + totalExpenses + '\n')
 }
 
-export {printReport, Expense, ExpenseType}
+export { printReport, Expense, ExpenseType }
