73327abb8db17be18418d045be09f06ff0b65add
David Vílchez <dvilchez@xuaps.com>
2022-06-22 20:20:02 +0200

    Fix test using a fixed date

--- src/ExpenseReport.test.ts
+++ src/ExpenseReport.test.ts
@@ -1,20 +1,20 @@
 import { printReport, Expense } from './ExpenseReport'
 
 describe(`ExpenseReport`, () => {
-    it(`should keep its original behavior`, () => {
-        let interceptedOutput = ""
-        jest.spyOn(process.stdout, "write").mockImplementation((output: string): boolean => {
-            interceptedOutput += output
-            return true;
-        })
-        printReport([
-          new Expense("dinner", 5001)
-        ])
-        expect(interceptedOutput).toEqual(
-`Expenses: 2022-06-07
+  it(`should keep its original behavior`, () => {
+    let interceptedOutput = ''
+    jest.spyOn(process.stdout, 'write').mockImplementation((output: string): boolean => {
+      interceptedOutput += output
+      return true
+    })
+    printReport([new Expense('dinner', 5001)])
+    expect(interceptedOutput).toEqual(
+      `Expenses: ${new Date().toISOString().substr(0, 10)}
 Dinner\t5001\tX
 Meal Expenses: 5001
 Total Expenses: 5001
-`)
-    })
+`,
+    )
+  })
 })
+
